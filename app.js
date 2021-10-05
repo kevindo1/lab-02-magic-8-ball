c3d42ad682706b268587f4aeec5765cffd70f0b5
Kevin Do <kevind0501@gmail.com>
2021-10-05 13:42:00 -0700

    added javascript so the images change depending on the answer given from the 8 ball

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 // import functions and grab DOM elements
+const userInput = document.getElementById('user-input');
+const submitButton = document.getElementById('submit-button');
+const outputAnswer = document.getElementById('output-answer');
+const displayPicture = document.getElementById('display-picture');
 
 // initialize global state
 
@@ -7,17 +11,40 @@
   // use user input to update state 
   // update DOM to reflect the new state
 
-const conchAnswers = ['Maybe someday.', 'Nothing.', 'Neither.', 'I dont think so.', 'No.', 'Yes.', 'Try asking again.'];
-
-const getRandomNumber = (max)=> {
-    return Math.floor(Math.random() * max);
-};
-
-const randomIndex = getRandomNumber(conchAnswers.length);
-console.log(randomIndex);
-console.log(conchAnswers[randomIndex]);
+const conchAnswers = ['Maybe someday.', 'Never.', 'Neither.', 'I dont think so.', 'No.', 'Yes.', 'Try asking again.'];
 
 // generate random response from array
 // generate random number thats equal to length of the array
 
+submitButton.addEventListener('click', ()=> {
+    const getRandomNumber = (max)=> {
+        return Math.floor(Math.random() * max);
+    };
+
+    const randomIndex = getRandomNumber(conchAnswers.length);
+    const randomAnswer = conchAnswers[randomIndex];
+    outputAnswer.textContent = randomAnswer;
+
+    if (outputAnswer.textContent === 'No.') {
+        document.getElementById('display-picture').src = './assets/scary.jpeg';
+    }
+    if (outputAnswer.textContent === 'Yes.') {
+        document.getElementById('display-picture').src = './assets/giggle.png';
+    }
+    if (outputAnswer.textContent === 'Never.') {
+        document.getElementById('display-picture').src = './assets/squidward.jpeg';
+    }
+    if (outputAnswer.textContent === 'Maybe someday.') {
+        document.getElementById('display-picture').src = './assets/squad.jpeg';
+    }
+    if (outputAnswer.textContent === 'Neither.') {
+        document.getElementById('display-picture').src = './assets/pull.jpeg';
+    }
+    if (outputAnswer.textContent === 'Try asking again.') {
+        document.getElementById('display-picture').src = './assets/yes.png';
+    }
+    if (outputAnswer.textContent === 'I dont think so.') {
+        document.getElementById('display-picture').src = './assets/funny.jpeg';
+    }
+});
 
