c4aef1c8a36a01720c1df7d5679fb817eb7bf656
Kevin Do <kevind0501@gmail.com>
2021-10-05 13:10:32 -0700

    Added HTML, CSS to magic 8 ball

--- app.js
+++ app.js
@@ -6,3 +6,18 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+const conchAnswers = ['Maybe someday.', 'Nothing.', 'Neither.', 'I dont think so.', 'No.', 'Yes.', 'Try asking again.'];
+
+const getRandomNumber = (max)=> {
+    return Math.floor(Math.random() * max);
+};
+
+const randomIndex = getRandomNumber(conchAnswers.length);
+console.log(randomIndex);
+console.log(conchAnswers[randomIndex]);
+
+// generate random response from array
+// generate random number thats equal to length of the array
+
+
